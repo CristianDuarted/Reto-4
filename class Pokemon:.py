e290216c0053663d95d1de6d6b23e0d92d79836f
CristianDuarted <cduarted@unal.edu.co>
2026-04-29 14:13:12 -0500

    Primer commit - ejercicio shapes completo

---
+++ class Pokemon:.py
@@ -0,0 +1,130 @@
+class Pokemon:
+    def __init__(self, types: str, name: str, life: int = 10,
+                 attack: int = 1, defense: float = 0.5,
+                 level: int = 1, special_ability: str = "None"):
+
+        self.types = types
+        self.name = name
+        self.life = life
+        self.attack_power = attack
+        self.defense = defense
+        self.level = level
+        self.special_ability = special_ability
+
+    def attack(self, objective, attack_power):
+        damage = attack_power * self.attack_power
+        objective.defender(damage)
+
+    def defender(self, damage):
+        damage_received = damage * (1 - self.defense)
+        self.life -= damage_received
+
+        if self.life < 0:
+            self.life = 0
+
+        print(f"{self.name} received {damage_received:.2f} damage. Life: {self.life}")
+
+    def evolucion(self, new_level, new_ability):
+        if new_level > self.level:
+            self.level = new_level
+            self.special_ability = new_ability
+            print(f"{self.name} evolved to level {self.level}")
+        else:
+            print("Cannot evolve to a lower level or the same level.")
+
+class TypeRelations:
+    def __init__(self):
+        self.diccionario = {
+            "Water": {
+                "Water": 0.0,
+                "Fire": 2.0,
+                "Earth": 0.5,
+                "Electric": 0.5,
+                "Air": 2.0,
+                "Steel": 2.0,
+                "Grass": 0.5,
+                "Poison": 2.0,
+            },
+            "Fire": {
+                "Water": 0.5,
+                "Fire": 0.0,
+                "Earth": 0.5,
+                "Electric": 2.0,
+                "Air": 2.0,
+                "Steel": 2.0,
+                "Grass": 2.0,
+                "Poison": 2.0,
+            },
+            "Earth": {
+                "Water": 2.0,
+                "Fire": 2.0,
+                "Earth": 0.0,
+                "Electric": 2.0,
+                "Air": 0.5,
+                "Steel": 2.0,
+                "Grass": 0.5,
+                "Poison": 2.0,
+            },
+            "Electric": {
+                "Water": 2.0,
+                "Fire": 0.5,
+                "Earth": 0.5,
+                "Electric": 0.0,
+                "Air": 2.0,
+                "Steel": 2.0,
+                "Grass": 0.5,
+                "Poison": 2.0,
+            },
+            "Air": {
+                "Water": 0.5,
+                "Fire": 0.5,
+                "Earth": 2.0,
+                "Electric": 0.5,
+                "Air": 0.0,
+                "Steel": 0.5,
+                "Grass": 2.0,
+                "Poison": 2.0,
+            },
+            "Steel": {
+                "Water": 0.5,
+                "Fire": 0.5,
+                "Earth": 0.5,
+                "Electric": 0.5,
+                "Air": 2.0,
+                "Steel": 0.0,
+                "Grass": 2.0,
+                "Poison": 2.0,
+            },
+            "Grass": {
+                "Water": 2.0,
+                "Fire": 0.5,
+                "Earth": 2.0,
+                "Electric": 0.5,
+                "Air": 0.5,
+                "Steel": 0.5,
+                "Grass": 0.0,
+                "Poison": 0.5,
+            },
+            "Poison":{
+                "Water": 0.5,
+                "Fire": 0.5,
+                "Earth": 0.5,
+                "Electric": 0.5,
+                "Air": 0.5,
+                "Steel": 0.5,
+                "Grass": 2.0,
+                "Poison": 0.0,
+            }
+        }
+    def get_effectiveness(self, attack_type: str, defender_types : list[str]) -> float:
+         multiplier = 1.0
+         for defender in defender_types:
+             if attack_type in self.diccionario:
+                 multiplier *= self.diccionario[attack_type][defender]
+         return multiplier
+    
+types = TypeRelations()
+
+print(types.get_effectiveness("Fire", ["Grass"]))          # 2.0
+print(types.get_effectiveness("Fire", ["Water"]))          # 0.5
+print(types.get_effectiveness("Fire", ["Grass", "Steel"])) # 4.0
--- class Rectangle:.py
+++ class Rectangle:.py
@@ -2,60 +2,139 @@ import math
 
 class Point:
     def __init__(self, x: float, y: float):
-        self.x = x
-        self.y = y
+        self._x = x
+        self._y = y
 
+    def get_x(self) -> int:
+        return self._x
     
+    def set_x(self, new_x:int):
+        self._x = new_x
+
+    def get_y(self) -> int:
+        return self._y
+    
+    def set_y(self, new_y:int):
+        self._x = new_y
+    
+    def __str__(self):
+        return f"({self._x}, {self._y})"
+    
+    def __repr__(self):
+        return f"Point({self._x}, {self._y})"
+
 class Line:
     def __init__ (self, start: Point, end: Point):
-        self.start = start
-        self.end = end
+        self._start = start
+        self._end = end
+    
+    def get_sart(self) -> int:
+        return self._start
+        
+    def set_start(self, new_start:int):
+        self._start = new_start
 
+    def get_end(self) -> int:
+        return self._end
+    
+    def set_end(self, new_end:int):
+        self._end = new_end
+
+
+    
     def compute_length(self):
-        lenght = math.sqrt((self.start.x - self.end.x ) ** 2 + (self.start.y - self.end.y) ** 2)
+        lenght = math.sqrt((self._start._x - self._end._x ) ** 2 + (self._start._y - self._end._y) ** 2)
         return lenght
     
     def compute_slope(self):
-       if self.end.x != self.start.x:
-            slope = (self.start.y - self.end.y) / (self.start.x - self.end.x)
+       if self._end._x != self._start._x:
+            slope = (self._start._y - self._end._y) / (self._start._x - self._end._x)
             angle = math.degrees(math.atan(slope))
             return angle
        else:
             return 90
     
     def compute_horizontal_cross(self) -> bool:
-        if (self.end.y <= 0 and self.start.y >= 0) or (self.end.y >= 0 and self.start.y <= 0):
+        if (self._end._y <= 0 and self._start._y >= 0) or (self._end._y >= 0 and self._start._y <= 0):
             return True
         else:
             return False
 
     def compute_vertical_cross(self) -> bool:  
-        if (self.end.x <= 0 and self.start.x >= 0) or (self.end.x >= 0 and self.start.x <= 0):
+        if (self._end._x <= 0 and self._start._x >= 0) or (self._end._x >= 0 and self._start._x <= 0):
             return True
         else:
             return False
+
+class Shape:
+    def __init__(self, is_regular: bool, vertices: list[Point], edges:list[Line], inner_angles: list[float]):
+        self._is_regular = is_regular
+        self._vertices = vertices
+        self._edges = edges
+        self._inner_angles = inner_angles
     
-class Rectangle:
+    def get_is_regular(self):
+        return self._is_regular
+    
+    def set_is_regular(self, new_is_regular):
+        self._new_is_regular = new_is_regular
+
+    def get_vertices(self):
+        return self._vertices
+    
+    def set_vertices(self, new_vertices):
+        self._vertices = new_vertices
+    
+    def get_edges(self):
+        return self._edges
+    
+    def set_edges(self, new_edges):
+        self._new_edges = new_edges
+
+    def get_inner_angles(self):
+        return self._inner_angles
+    
+    def set_inner_angles(self, new_inner_angles):
+        self._inner_angles = new_inner_angles
+
+    def compute_area(self):
+        pass
+
+    def compute_perimeter(self)->float:
+        perimeter = 0
+        for edge in self._edges:
+            perimeter += edge.compute_length()
+        return perimeter    
+
+    def compute_inner_angles(self):
+        pass    
+        
+
+
+
+class Rectangle(Shape):
     def __init__(self, **kwargs):
+        
         if "width" in kwargs and "height" in kwargs and "center" in kwargs:
-            self.width = kwargs["width"] 
-            self.height = kwargs["height"]
-            self.center = kwargs["center"]        
+            self._width = kwargs["width"] 
+            self._height = kwargs["height"]
+            self._center = kwargs["center"]
+         
         elif "point1" in kwargs and "point2" in kwargs:
             self.point1 = kwargs["point1"]
             self.point2 = kwargs["point2"]
-            self.width = abs(self.point2.x - self.point1.x)
-            self.height = abs(self.point2.y - self.point1.y)
-            center_x = (self.point1.x + self.point2.x) / 2
-            center_y = (self.point1.y + self.point2.y) / 2
-            self.center = Point(center_x, center_y)
+            self._width = abs(self.point2._x - self.point1._x)
+            self._height = abs(self.point2._y - self.point1._y)
+            center_x = (self.point1._x + self.point2._x) / 2
+            center_y = (self.point1._y + self.point2._y) / 2
+            self._center = Point(center_x, center_y)
         elif "width" in kwargs and "height" in kwargs and "bottom_left" in kwargs:
-            self.width = kwargs["width"] 
-            self.height = kwargs["height"]
+            self._width = kwargs["width"] 
+            self._height = kwargs["height"]
             self.bottom_left = kwargs["bottom_left"]
-            center_x = (self.bottom_left.x + self.width /2)
-            center_y = (self.bottom_left.y + self.height /2)
-            self.center = Point(center_x, center_y)
+            center_x = (self.bottom_left._x + self.width /2)
+            center_y = (self.bottom_left._y + self.height /2)
+            self._center = Point(center_x, center_y)
 
         elif "line1" in kwargs and "line2" in kwargs and "line3" in kwargs and "line4" in kwargs:
             self.line1 = kwargs ["line1"]
@@ -64,152 +143,236 @@ class Rectangle:
             self.line4 = kwargs ["line4"]
             if (self.line1.compute_length() == self.line2.compute_length()):
                 if (self.line4.compute_length() == self.line3.compute_length()):
-                    self.width = self.line1.compute_length()
-                    self.height = self.line3.compute_length()
-                    center_x = ((self.line1.start.x + self.line2.end.x) /2)
-                    center_y = ((self.line1.end.y+ self.line2.start.y) /2)
-                    self.center = Point(center_x, center_y)
+                    self._width = self.line1.compute_length()
+                    self._height = self.line3.compute_length()
+                    center_x = ((self.line1._start._x + self.line2._end._x) /2)
+                    center_y = ((self.line1._end._y+ self.line2._start._y) /2)
+                    self._center = Point(center_x, center_y)
                 else:
                     return ValueError("Parámetros inválidos")
             elif (self.line1.compute_length() == self.line3.compute_length()):
                  if (self.line2.compute_length() == self.line4.compute_length()):
-                    self.width = self.line1.compute_length()
-                    self.height = self.line4.compute_length()
-                    center_x = ((self.line1.end.x + self.line3.start.x) /2)
-                    center_y = ((self.line1.start.y + self.line3.end.y) /2)
-                    self.center = Point(center_x, center_y)
+                    self._width = self.line1.compute_length()
+                    self._height = self.line4.compute_length()
+                    center_x = ((self.line1._end._x + self.line3._start._x) /2)
+                    center_y = ((self.line1._start._y + self.line3._end._y) /2)
+                    self._center = Point(center_x, center_y)
             elif (self.line1.compute_length() == self.line4.compute_length()):
                 if (self.line2.compute_length() == self.line3.compute_length()):
-                    self.width = self.line1.compute_length()
-                    self.height = self.line3.compute_length()
-                    center_x = ((self.line1.start.x + self.line4.start.x) /2)
-                    center_y = ((self.line2.end.y + self.line4.end.y) /2)
-                    self.center = Point(center_x, center_y)
+                    self._width = self.line1.compute_length()
+                    self._height = self.line3.compute_length()
+                    center_x = ((self.line1._start._x + self.line4._start._x) /2)
+                    center_y = ((self.line2._end._y + self.line4._end._y) /2)
+                    self._center = Point(center_x, center_y)
             else:
                 ValueError("Parámetros inválidos")
-       
+        
         else:
              raise ValueError("Parámetros inválidos")
-       
+        half_width = self._width / 2
+        half_height = self._height / 2
 
-    def compute_area(self) -> str:
-        return f"el area es: {self.width * self.height}"
+        def get_width(self):
+            return self._width 
 
-    def compute_perimeter(self) -> str:
-        return f"el perimetro es: {(self.width + self.height)*2}"
+        def set_width(self, new_width):
+            self._width = new_width
+
+        def get_height(self):
+            return self._height
+            
+        def set_height(self, new_height):
+            self._height = new_height
+
+        def get_center(self):
+            return self._center
+            
+        def set_center(self, new_center):
+            self._center = new_center 
+
+        p1 = Point(self._center._x - half_width, self._center._y - half_height)
+        p2 = Point(self._center._x + half_width, self._center._y - half_height)
+        p3 = Point(self._center._x + half_width, self._center._y + half_height)
+        p4 = Point(self._center._x - half_width, self._center._y + half_height)
+
+        vertices = [p1, p2, p3, p4] 
+
+        edges = [
+        Line(p1, p2),
+        Line(p2, p3),
+        Line(p3, p4),
+        Line(p4, p1)
+        ]
+
+        inner_angles = [90, 90, 90, 90]
+
+        is_regular = self._width == self._height  
+
+        super().__init__(is_regular, vertices, edges, inner_angles)
+
+
+    def compute_area(self) -> float:
+        return self._width * self._height
+
+
     
     def __str__(self):
-        return f"Rectangle(width = {self.width}, height = {self.height}, center = ({self.center.x}, {self.center.y}))"
-    
+        return f"Rectangle(width = {self._width}, height = {self._height}, center = ({self._center._x}, {self._center._y}))"
 
-
-p1 = Point(2, 3)
-p2 = Point(8, 7)
-
-r1 = Rectangle(width=10, height=5, center=Point(0,0))
-r2 = Rectangle(point1=p1, point2=p2)
-r3 = Rectangle(width=10, height=5, bottom_left=p1)
-
-print(r2.compute_perimeter())
-print(r2)
-print(r2.compute_area())
+class Square(Rectangle):
+    def __init__(self, side, center):
+        super().__init__(width=side, height=side, center=center)
 
 
 
+class Triangle(Shape):
+    def __init__(self,**kwargs):       
+        p1 = kwargs["vertice1"]
+        p2 = kwargs["vertice2"]
+        p3 = kwargs["vertice3"]
+        vertices = [p1,p2,p3]
+        self.line1= Line(p1,p2)
+        self.line2= Line(p2,p3)
+        self.line3= Line(p3,p1)
+        edges = [self.line1,self.line2,self.line3]
+        a = self.line1.compute_length()
+        b = self.line2.compute_length()
+        c = self.line3.compute_length()
+        if (round(a,5) == round(b,5) == round(c,5)):
+            is_regular = True
+        else:
+          is_regular = False
+        inner_angles = []    
+        super().__init__(is_regular, vertices, edges, inner_angles)
+    def compute_area(self) -> float:
+        #formula de heron
+        a = self.line1.compute_length()
+        b = self.line2.compute_length()
+        c = self.line3.compute_length()
 
-print("=== PRUEBAS POINT ===")
-p1 = Point(2, 3)
-p2 = Point(8, 7)
+        semiperimetro = (a + b + c) / 2
+        area = math.sqrt(semiperimetro*
+                         (semiperimetro - a)*
+                         (semiperimetro - b)*
+                         (semiperimetro - c))
+        return area
 
-print(p1.x)
-print(p1.y)
-print(p2.x)
-print(p2.y)
+class Equilateral(Triangle):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
+        a = self.line1.compute_length()
+        b = self.line2.compute_length()
+        c = self.line3.compute_length()
 
-print("\n=== PRUEBAS LINE ===")
-line1 = Line(Point(2, 3), Point(8, 7))
-print(line1.compute_length())
-print(line1.compute_slope())
-print(line1.compute_horizontal_cross())
-print(line1.compute_vertical_cross())
+        if not (round(a, 5) == round(b, 5) == round(c, 5)):
+            raise ValueError("No es un triángulo equilátero")
+        
+        
+class Isosceles(Triangle):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-line2 = Line(Point(2, 3), Point(5, -2))
-print(line2.compute_horizontal_cross())
-print(line2.compute_vertical_cross())
+        a = self.line1.compute_length()
+        b = self.line2.compute_length()
+        c = self.line3.compute_length()
 
-line3 = Line(Point(-2, 3), Point(4, 5))
-print(line3.compute_horizontal_cross())
-print(line3.compute_vertical_cross())
+        if not (a == b or b == c or a == c):
+            raise ValueError("No es un triángulo isósceles")
 
-line4 = Line(Point(2, 1), Point(2, 8))
-print(line4.compute_length())
-print(line4.compute_slope())
-print(line4.compute_horizontal_cross())
-print(line4.compute_vertical_cross())
+class Scalene(Triangle):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-line5 = Line(Point(1, 4), Point(7, 4))
-print(line5.compute_length())
-print(line5.compute_slope())
-print(line5.compute_horizontal_cross())
-print(line5.compute_vertical_cross())
+        a = self.line1.compute_length()
+        b = self.line2.compute_length()
+        c = self.line3.compute_length()
 
-line6 = Line(Point(-2, 0), Point(5, 0))
-print(line6.compute_horizontal_cross())
-print(line6.compute_vertical_cross())
+        if (a == b or b == c or a == c):
+            raise ValueError("No es un triángulo escaleno")
+        
+class TriRectangle(Triangle):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
+        a = self.line1.compute_length()
+        b = self.line2.compute_length()
+        c = self.line3.compute_length()
 
-print("\n=== PRUEBAS RECTANGLE ===")
-r1 = Rectangle(width=10, height=5, center=Point(0, 0))
-print(r1)
-print(r1.compute_area())
-print(r1.compute_perimeter())
-print(r1.width)
-print(r1.height)
-print(r1.center.x)
-print(r1.center.y)
+        lados = sorted([a, b, c])
 
-r2 = Rectangle(point1=Point(2, 3), point2=Point(8, 7))
-print(r2)
-print(r2.compute_area())
-print(r2.compute_perimeter())
-print(r2.width)
-print(r2.height)
-print(r2.center.x)
-print(r2.center.y)
-
-r3 = Rectangle(width=10, height=5, bottom_left=Point(2, 3))
-print(r3)
-print(r3.compute_area())
-print(r3.compute_perimeter())
-print(r3.width)
-print(r3.height)
-print(r3.center.x)
-print(r3.center.y)
-
-a = Point(0, 0)
-b = Point(6, 0)
-c = Point(6, 4)
-d = Point(0, 4)
-
-l1 = Line(a, b)
-l2 = Line(b, c)
-l3 = Line(c, d)
-l4 = Line(d, a)
-
-r4 = Rectangle(line1=l1, line2=l2, line3=l3, line4=l4)
-print(r4)
-print(r4.compute_area())
-print(r4.compute_perimeter())
-print(r4.width)
-print(r4.height)
-print(r4.center.x)
-print(r4.center.y)
+        if not (lados[0]**2 + lados[1]**2 == lados[2]**2):
+            raise ValueError("No es un triángulo rectángulo")
 
 
-print("\n=== PRUEBA ERROR ===")
-try:
-    malo = Rectangle(a=1, b=2)
-    print(malo)
-except Exception as e:
-    print(type(e).__name__, e)
+print("=== RECTANGLE ===")
+r = Rectangle(width=10, height=5, center=Point(0, 0))
+
+print(f"Área rectángulo: {r.compute_area()}")
+print(f"Perímetro rectángulo: {r.compute_perimeter()}")
+print(f"Vértices rectángulo: {r.get_vertices()}")
+
+
+print("\n=== SQUARE ===")
+s = Rectangle(width=4, height=4, center=Point(0, 0))
+
+print(f"Área cuadrado: {s.compute_area()}")
+print(f"Perímetro cuadrado: {s.compute_perimeter()}")
+print(f"¿Es regular?: {s.get_is_regular()}")
+
+
+print("\n=== TRIANGLE 3-4-5 ===")
+t = Triangle(
+    vertice1=Point(0, 0),
+    vertice2=Point(4, 0),
+    vertice3=Point(0, 3)
+)
+
+print(f"Área triángulo: {t.compute_area()}")
+print(f"Perímetro triángulo: {t.compute_perimeter()}")
+print(f"Vértices triángulo: {t.get_vertices()}")
+
+
+print("\n=== TRI-RECTANGLE ===")
+tr = TriRectangle(
+    vertice1=Point(0, 0),
+    vertice2=Point(4, 0),
+    vertice3=Point(0, 3)
+)
+
+print(f"Área triángulo rectángulo: {tr.compute_area()}")
+print(f"Perímetro triángulo rectángulo: {tr.compute_perimeter()}")
+
+
+print("\n=== ISOSCELES ===")
+iso = Isosceles(
+    vertice1=Point(0, 0),
+    vertice2=Point(4, 0),
+    vertice3=Point(2, 3)
+)
+
+print(f"Área isósceles: {iso.compute_area()}")
+print(f"Perímetro isósceles: {iso.compute_perimeter()}")
+
+
+print("\n=== SCALENE ===")
+sca = Scalene(
+    vertice1=Point(0, 0),
+    vertice2=Point(4, 0),
+    vertice3=Point(1, 3)
+)
+
+print(f"Área escaleno: {sca.compute_area()}")
+print(f"Perímetro escaleno: {sca.compute_perimeter()}")
+
+
+print("\n=== EQUILATERAL ===")
+eq = Equilateral(
+    vertice1=Point(0, 0),
+    vertice2=Point(2, 0),
+    vertice3=Point(1, math.sqrt(3))
+)
+
+print(f"Área equilátero: {eq.compute_area()}")
+print(f"Perímetro equilátero: {eq.compute_perimeter()}")
+print(f"¿Es regular?: {eq.get_is_regular()}")
